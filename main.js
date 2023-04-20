fd4e9f61b7e92a4ac2cd6ca7a6ddaa782f100342
Jonathan Pena-Loa <jpena-loa@LABA.PSWAS.LOCAL>
2023-04-20 14:57:36 -0700

    wired the event listener to the form for the input and textarea

--- main.js
+++ main.js
@@ -1,2 +1,44 @@
-// console.log("Hello")
 
+
+let formIElement = document.querySelector('form');
+
+formIElement.addEventListener('submit', (event) => {
+    event.preventDefault();
+    let todoItemFromForm = formIElement.elements['todo-item'];
+    let todoDescriptionFromForm = formIElement.elements['description'];
+    console.log(todoItemFromForm.value);
+    console.log(todoDescriptionFromForm.value);
+})
+
+
+
+class TodoItem {
+    constructor(id, text, isCompleted = false, element) {
+        this.id = id;
+        this.text = text;
+        this.isCompleted = isCompleted;
+        this.element = element;
+    }
+    createDOMElement() {
+        
+    }
+    updateDOMElement() {
+
+    }
+}
+
+
+class TodoList {
+    constructor(todoItem, todoDescription) {
+
+    }
+    add(text) {
+
+    }
+    remove(id) {
+
+    }
+    update(id, updates) {
+
+    }
+}
